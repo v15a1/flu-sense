8d45d2ea59a9c5049500c3ba04d172f6cef92a24
Visal Rajapakse <visalrajapakse@gmail.com>
2020-08-01 23:35:52 +0530

    first commit

---
+++ flu-sense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flu-sense
+//
+//  Created by Visal Rajapakse on 8/1/20.
+//  Copyright © 2020 Visal Rajapakse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
